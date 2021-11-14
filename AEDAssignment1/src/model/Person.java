0bcd06518ea11e352f5600f96a0c3d54c46593a8
Harshhal jaiswal <harshaljaiswal25@gmail.com>
2021-11-14 01:20:07 -0500

    updated assignments

---
+++ AEDAssignment1/src/model/Person.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Image;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ *
+ * @author harshaljaiswal
+ */
+public class Person {
+    private String name;
+    private String geoData;
+    private String dob;
+    private String telNumber;
+    private String faxNumber;
+    private String email;
+    private String ssn;
+    private String medRecNumber;
+    private String healthPlanNumber;
+
+    public String getHealthPlanNumber() {
+        return healthPlanNumber;
+    }
+
+    public void setHealthPlanNumber(String healthPlanNumber) {
+        this.healthPlanNumber = healthPlanNumber;
+    }
+    private String bankAccNumber;
+    private String certfNumber;
+    private String licencePlate;
+    private String serialNo;
+    private String linkedIn;
+    private String internetProtocolAdd;
+    private String biomerticIdentif;
+    private String face;
+    private String idCode;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGeoData() {
+        return geoData;
+    }
+
+    public void setGeoData(String geoData) {
+        this.geoData = geoData;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getTelNumber() {
+        return telNumber;
+    }
+
+    public void setTelNumber(String telNumber) {
+        this.telNumber = telNumber;
+    }
+
+    public String getFaxNumber() {
+        return faxNumber;
+    }
+
+    public void setFaxNumber(String faxNumber) {
+        this.faxNumber = faxNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getMedRecNumber() {
+        return medRecNumber;
+    }
+
+    public void setMedRecNumber(String medRecNumber) {
+        this.medRecNumber = medRecNumber;
+    }
+
+    public String getBankAccNumber() {
+        return bankAccNumber;
+    }
+
+    public void setBankAccNumber(String bankAccNumber) {
+        this.bankAccNumber = bankAccNumber;
+    }
+
+    public String getCertfNumber() {
+        return certfNumber;
+    }
+
+    public void setCertfNumber(String certfNumber) {
+        this.certfNumber = certfNumber;
+    }
+
+    public String getLicencePlate() {
+        return licencePlate;
+    }
+
+    public void setLicencePlate(String licencePlate) {
+        this.licencePlate = licencePlate;
+    }
+
+    public String getSerialNo() {
+        return serialNo;
+    }
+
+    public void setSerialNo(String serialNo) {
+        this.serialNo = serialNo;
+    }
+
+    public String getLinkedIn() {
+        return linkedIn;
+    }
+
+    public void setLinkedIn(String linkedIn) {
+        this.linkedIn = linkedIn;
+    }
+
+    public String getInternetProtocolAdd() {
+        return internetProtocolAdd;
+    }
+
+    public void setInternetProtocolAdd(String internetProtocolAdd) {
+        this.internetProtocolAdd = internetProtocolAdd;
+    }
+
+    public String getBiomerticIdentif() {
+        return biomerticIdentif;
+    }
+
+    public void setBiomerticIdentif(String biomerticIdentif) {
+        this.biomerticIdentif = biomerticIdentif;
+    }
+
+    public String getFace() {
+        return face;
+    }
+
+    public void setFace(String face) {
+        this.face = face;
+    }
+
+    public String getIdCode() {
+        return idCode;
+    }
+
+    public void setIdCode(String idCode) {
+        this.idCode = idCode;
+    }
+    
+    
+    
+}
